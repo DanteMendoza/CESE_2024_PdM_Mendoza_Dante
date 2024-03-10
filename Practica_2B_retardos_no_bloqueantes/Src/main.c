3563834e46d62e4cdd0780c12452c7c53084946e
DanteMendoza <31194413+DanteMendoza@users.noreply.github.com>
2024-03-10 00:05:19 -0300

    Practica2_v3

--- Practica_2B_retardos_no_bloqueantes/Src/main.c
+++ Practica_2B_retardos_no_bloqueantes/Src/main.c
@@ -35,20 +35,14 @@ void delayInit(delay_t *delay, tick_t duration) {
           // Cambiar el flag a 'true'
           delay->running = true;
           return false;
-      } else {
-          // Calcular el tiempo transcurrido
-          tick_t currentTime = HAL_GetTick();
-          tick_t elapsedTime = currentTime - delay->startTime;
-
-          // Verificar si se ha cumplido el tiempo del retardo
-          if (elapsedTime >= delay->duration) {
-              // Cambiar el flag a 'false'
-              delay->running = false;
-              return true;  // El tiempo se cumplió
-          } else {
-              return false;  // El tiempo no se ha cumplido
-          }
       }
+
+      if (delay->duration <= (HAL_GetTick() - delay->startTime)) {
+    	  delay->running = false;
+          return true;  // El tiempo se cumplió
+      }
+
+      return false;  // El tiempo no se ha cumplido
   }
 
   void delayWrite(delay_t *delay, tick_t duration) {
@@ -68,42 +62,22 @@ int main(void)
 
   // Inicializar LEDs usando BSP
   BSP_LED_Init(LED1); // Verde.
-  BSP_LED_Init(LED2); // Azul.
-  BSP_LED_Init(LED3); // Rojo.
 
   // Apagamos todos los leds previamente.
   BSP_LED_Off(LED1);
-  BSP_LED_Off(LED2);
-  BSP_LED_Off(LED3);
 
   // Inicializar estructuras de retardos para cada LED
-  delay_t delayLED1, delayLED2, delayLED3;
+  delay_t delayLED1;
   delayInit(&delayLED1, 100);
-  delayInit(&delayLED2, 100);
-  delayInit(&delayLED3, 100);
 
   /* Infinite loop */
   while (1)
   {
 		// Verificar y manejar retardos para LED1
 		if (delayRead(&delayLED1)) {
-			BSP_LED_On(LED1);  // Encender LED1 (verde)
-		} else {
-			BSP_LED_Off(LED1);  // Apagar LED1 (verde)
-		}
-
-		// Verificar y manejar retardos para LED2
-		if (delayRead(&delayLED2)) {
-			BSP_LED_On(LED2);  // Encender LED2 (azul)
-		} else {
-			BSP_LED_Off(LED2);  // Apagar LED2 (azul)
-		}
-
-		// Verificar y manejar retardos para LED3
-		if (delayRead(&delayLED3)) {
-			BSP_LED_On(LED3);  // Encender LED3 (rojo)
-		} else {
-			BSP_LED_Off(LED3);  // Apagar LED3 (rojo)
+			// LED1 (verde)
+			BSP_LED_Toggle(LED1);
+			delayInit(&delayLED1, 100);
 		}
   }
 }
