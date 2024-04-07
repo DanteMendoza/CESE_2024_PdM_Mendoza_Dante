c0d0dcbbb2ace9ce0d69712a9ac8ee9e7bd6dfd5
Dante Mendoza <dantehmendoza@gmail.com>
2024-04-07 13:02:18 -0300

    TP_Final_v0.1

---
+++ TP_Final/Debug/Drivers/API/Src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/API/Src/hc_sr04.c \
+../Drivers/API/Src/liquidcrystal_i2c.c 
+
+OBJS += \
+./Drivers/API/Src/hc_sr04.o \
+./Drivers/API/Src/liquidcrystal_i2c.o 
+
+C_DEPS += \
+./Drivers/API/Src/hc_sr04.d \
+./Drivers/API/Src/liquidcrystal_i2c.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/API/Src/%.o Drivers/API/Src/%.su Drivers/API/Src/%.cyclo: ../Drivers/API/Src/%.c Drivers/API/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/Proyectos/STM32CubeIDE/CESE_2024_PdM_Mendoza_Dante/TP_Final/Drivers/API/Inc" -I"C:/Proyectos/STM32CubeIDE/CESE_2024_PdM_Mendoza_Dante/TP_Final/Drivers/API/Src" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-API-2f-Src
+
+clean-Drivers-2f-API-2f-Src:
+	-$(RM) ./Drivers/API/Src/hc_sr04.cyclo ./Drivers/API/Src/hc_sr04.d ./Drivers/API/Src/hc_sr04.o ./Drivers/API/Src/hc_sr04.su ./Drivers/API/Src/liquidcrystal_i2c.cyclo ./Drivers/API/Src/liquidcrystal_i2c.d ./Drivers/API/Src/liquidcrystal_i2c.o ./Drivers/API/Src/liquidcrystal_i2c.su
+
+.PHONY: clean-Drivers-2f-API-2f-Src
+
